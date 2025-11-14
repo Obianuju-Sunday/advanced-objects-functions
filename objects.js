9459bb05bf08eab15b841cf27c4736eb3dfaa6d9
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-11-14 13:33:39 +0100

    first commit. Objects literals and object constructors

---
+++ objects.js
@@ -0,0 +1,28 @@
+// Object Literal
+var john = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+}
+
+// Creating an Object using function Constructor
+// Method 1 - Using Modern ES6 Class Syntax (Commented out duplicate for clarity)
+// class Person {
+//     constructor(firstName, lastName, age) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+// }
+
+// Method 2 - Traditional Function Constructor Syntax
+// function Person(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+// }
+
+// var jane = new Person("Jane", "Doe", 25);
+
+// console.log(jane);  
+
