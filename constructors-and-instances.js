4a3ef1a85162570b196f8e0a183f0da2ddc70968
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-11-14 13:45:34 +0100

    created an age property on the object

--- constructors-and-instances.js
+++ constructors-and-instances.js
@@ -9,7 +9,7 @@ class Person {
   celebrateBirthday(currentYear){
     var age = currentYear - this.yearOfBirth;
     console.log(`Happy birthday ${this.name}. You are ${age} years old now.`);
-   // return age;
+    this.age = age;
   }
 }
 
@@ -20,3 +20,6 @@ var ngozi = new Person('Ngozi', 2005);
 // Testing the celebrateBirthday method
 ngozi.celebrateBirthday(2025); // Output: Happy birthday Ngozi. You are 20 years old now.
 obianuju.celebrateBirthday(2025); // Output: Happy birthday Obianuju. You are 22 years old now.
+
+console.log(obianuju);
+console.log(ngozi);
