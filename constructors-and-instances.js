c9c01c53f7246ae90062a4201600701914877876
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-11-14 13:36:08 +0100

    Add Person class with constructor, instances, and birthday method

---
+++ constructors-and-instances.js
@@ -0,0 +1,22 @@
+
+// Personal Challenge 
+class Person {
+  constructor(name, yearOfBirth){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+  }
+  
+  celebrateBirthday(currentYear){
+    var age = currentYear - this.yearOfBirth;
+    console.log(`Happy birthday ${this.name}. You are ${age} years old now.`);
+   // return age;
+  }
+}
+
+// Creating instances of Person
+var obianuju = new Person('Obianuju', 2003);
+var ngozi = new Person('Ngozi', 2005);
+
+// Testing the celebrateBirthday method
+ngozi.celebrateBirthday(2025); // Output: Happy birthday Ngozi. You are 20 years old now.
+obianuju.celebrateBirthday(2025); // Output: Happy birthday Obianuju. You are 22 years old now.
