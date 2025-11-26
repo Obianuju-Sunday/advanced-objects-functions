9e0b5a92afe235fa9147c585c51f6ef654558125
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-11-26 21:30:57 +0100

    minor code changes

--- IIFEs.js
+++ IIFEs.js
@@ -19,59 +19,63 @@
 
 // Application Configuration using IIFE
 (function () {
-  const config = {
-    theme: "dark",
-    mode: "beginner",
-    user: "Obianuju"
-  };
+    const config = {
+        theme: "dark",
+        mode: "beginner",
+        user: "Obianuju"
+    };
 
-  function toggleTheme() {
-    if (config.theme === "dark"){
-      config.theme = "light";
-      console.log("Theme changed to light.");
-    } else {
-      config.theme === "dark";
-      console.log("Theme changed to dark.");
+    function toggleTheme() {
+        if (config.theme === "dark") {
+            config.theme = "light";
+            console.log("Theme changed to light.");
+        } else {
+            config.theme === "dark";
+            console.log("Theme changed to dark.");
+        }
+        return config.theme;
     }
-    return config.theme;
-  }
 
-  toggleTheme();
+    toggleTheme();
 
-  console.log(`Welcome ${config.user}! Your app is set to ${config.theme} theme in ${config.mode} mode.`);
+    console.log(`Welcome ${config.user}! Your app is set to ${config.theme} theme in ${config.mode} mode.`);
 })();
 
-// Score Tracker using IIFE
-
-(function(){
-  var counter = 0;
-  
-  function increaseCounter(){
-    counter += 1;
-    console.log(counter);
-  }
-  
-  function showCounter(){
-    console.log(`Current score: ${counter}`)
-  }
-  
-  increaseCounter();
-  showCounter();
-})();
 
 // Discount System using IIFE
-(function(){
+(function () {
     var originalPrice = 6800;
     var discountPercent = 40;
 
-    function calculateDiscount(){
-        return discountPrice = originalPrice - (originalPrice * discountPercent / 100)
+    function calculateDiscount() {
+        return discountPrice = originalPrice - (originalPrice * discountPercent / 100);
     }
 
-    function showDiscount(){
+    function showDiscount() {
         calculateDiscount();
-        console.log(`After ${discountPercent}% discount, the new price to be paid at the checkout is ${discountPrice}.`)
+        console.log(`After ${discountPercent}% discount, the new price to be paid at the checkout is $${discountPrice}.`)
     }
 
     showDiscount();
+    console.log(discountPrice)
+
+})();
+
+
+// Score Tracker using IIFE
+
+(function () {
+    var counter = 0;
+
+    function increaseCounter() {
+        counter += 1;
+        console.log(counter);
+    }
+
+    function showCounter() {
+        console.log(`Current score: ${counter}`)
+    }
+
+    increaseCounter();
+    showCounter();
 })();
