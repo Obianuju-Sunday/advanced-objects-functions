d02b6a4ef208a29f99ec574242b29be0d9416cad
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-12-01 14:05:06 +0100

    the bind() method

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -24,3 +24,42 @@ var watchProduct = {
 }
 
 shoeProduct.calculateDiscount.apply(watchProduct, [25]); // Using apply to borrow method for watchProduct
+
+var tvProduct = {
+    brand: 'Samsung TV',
+    price: 2000
+}
+
+var tvDiscount = shoeProduct.calculateDiscount.bind(tvProduct, 15); // Using bind to create a new function for tvProduct
+tvDiscount(); // Calling the bound function
+
+
+
+
+
+
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2025 - el;
+}
+
+function isOfFullAge(limit, el){
+  return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log('Ages:', ages);
+
+var fullNigeria = arrayCalc(ages, isOfFullAge.bind(this, 18));
+console.log('Full ages in Nigeria (18+):', fullNigeria);
