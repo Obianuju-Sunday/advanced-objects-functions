437aee25b29fdee7da32b3f54321e1d48d67b12a
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-12-01 16:47:08 +0100

    chose and displayed question

--- section-challenge.js
+++ section-challenge.js
@@ -4,9 +4,16 @@ class Question {
         this.possibleAnswers = possibleAnswers;
         this.correctAnswer = correctAnswer;
     }
+
+    displayQuestion() {
+        console.log(this.question);
+        for (var i = 0; i < this.possibleAnswers.length; i++) {
+            console.log(this.possibleAnswers[i]);
+        }
+    }
 }
 
-var question1 = new Question('What does a closure allow a function to do?', '\n A. Access variables from its outer function even after that outer function has finished running \n B. Remove variables from memory \n C. Create global variables \n D. Stop the call stack', 'A');
+var question1 = new Question('What does a closure allow a function to do?', ['\n A. Access variables from its outer function even after that outer function has finished running \n B. Remove variables from memory \n C. Create global variables \n D. Stop the call stack'], 'A');
 
 var question2 = new Question('Which method can be used to borrow a function from another object?', ['\n A. call() \n B. bind() \n C. apply() \n D. All of the above'], 'D');
 
@@ -16,7 +23,7 @@ var question4 = new Question('What is the main purpose of using IIFE (Immediatel
 
 var question5 = new Question('Which of the following is NOT a characteristic of JavaScript functions?', ['\n A. First-class citizens \n B. Can be assigned to variables \n C. Cannot be passed as arguments \n D. Can return other functions'], 'C');
 
-var questionsix = new Question('Which method permanently locks the value of this for a function?', ['\n A. call() \n B. apply() \n C. bind() \n D. freeze()'], 'C');
+var question6 = new Question('Which method permanently locks the value of this for a function?', ['\n A. call() \n B. apply() \n C. bind() \n D. freeze()'], 'C');
 
 var question7 = new Question('What is the output of the following code snippet?\n\n(function() {\n  var x = 10;\n  return function() {\n    console.log(x);\n  };\n})();\n', ['\nA. undefined \nB. 10 \nC. Error \nD. null'], 'B');
 
@@ -26,21 +33,20 @@ var question9 = new Question('Which fabric is known for its soft, luxurious feel
 
 var question10 = new Question('What is the term for a fashion style that combines elements from different cultures?', ['A. Fusion \nB. Vintage \nC. Bohemian \nD. Minimalist'], 'A');
 
-var question11 = new Question('Which brand is famous for its red-soled heels?', ['A. Christian Louboutin \nB. Jimmy Choo \nC. Manolo Blahnik \nD. Gucci'], 'A');
+var question11 = new Question('Which brand is famous for its red-soled heels?', ['A. Christian Louboutin \n B. Jimmy Choo \nC. Manolo Blahnik \nD. Gucci'], 'A');
 
-var question12 = new Question('What is the name of the fashion event held annually in New York City?', ['A. New York Fashion Week \nB. Paris Fashion Week \nC. Milan Fashion Week \nD. London Fashion Week'], 'A'); 
-
-var question13 = new Question('What is monochrome fashion?', ['A. Wearing different shades of a single color \nB. Wearing black and white only \nC. Wearing patterns and prints \nD. Wearing bright colors'], 'A'); 
-
+var question12 = new Question('What is the name of the fashion event held annually in New York City?', ['A. New York Fashion Week \n B. Paris Fashion Week \n C. Milan Fashion Week \n D. London Fashion Week'], 'A');
+// console.log(question12);
+var question13 = new Question('What is monochrome fashion?', ['A. Wearing different shades of a single color, \nB. Wearing black and white only, \nC. Wearing patterns and prints, \nD. Wearing bright colors'], 'A'); 
 
 // An array to hold all questions
 var questionBank = [
-    question1, 
-    question2, 
-    question3, 
-    question4, 
-    question5, 
-    questionsix,
+    question1,
+    question2,
+    question3,
+    question4,
+    question5,
+    question6,
     question7,
     question8,
     question9,
@@ -49,3 +55,16 @@ var questionBank = [
     question12,
     question13
 ];
+
+// for (var i = 0; i < questionBank.length; i++) {
+//     console.log(questionBank[i]);
+// }
+
+
+
+var randomIndex = Math.floor(Math.random() * 13);
+var randomQuestion = questionBank[randomIndex];
+randomQuestion.displayQuestion();
+// console.log(randQuestion)
+// console.log(questionBank[randIndex].question);
+// console.log(questionBank[randIndex].possibleAnswers);
